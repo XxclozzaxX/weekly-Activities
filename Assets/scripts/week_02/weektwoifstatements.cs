c0dbee8475c16075303434d8ec3e1676b8c58723
chloe buckley <1017279@student.sae.edu.au>
2020-06-24 17:54:57 +1000

    few more things from week 2 class

--- Assets/scripts/week_02/weektwoifstatements.cs
+++ Assets/scripts/week_02/weektwoifstatements.cs
@@ -7,7 +7,6 @@ public class weektwoifstatements : MonoBehaviour
 
     public float myFloat;
 
-
     // Start is called before the first frame update
     void Start()
     {
@@ -27,25 +26,22 @@ public class weektwoifstatements : MonoBehaviour
         {
             Debug.Log("the number was less than 5");
         }
-        
+
         if (myFloat > 3 || myFloat < 5)
+        {
+            Debug.Log("The number was less than 3 " +
+                "or the number was greater than 5");
+        }
+        else
         {
             Debug.Log("the number was exactly 4");
         }
 
-        if (myFloat > 1 || myFloat < 5)
+        if (myFloat > 1 && myFloat < 5 || myFloat > 7)
         {
             Debug.Log("the number was greater than 1 and " +
                 "less than 5 or the number was greater " +
                 "than 7");
         }
-
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
