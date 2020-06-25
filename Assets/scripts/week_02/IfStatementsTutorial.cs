1f14765e6c1aebf719fa03c9093c2f3f5c80f8ab
chloe buckley <1017279@student.sae.edu.au>
2020-06-25 14:02:27 +1000

    if statements tutorial completed

--- Assets/scripts/week_02/IfStatementsTutorial.cs
+++ Assets/scripts/week_02/IfStatementsTutorial.cs
@@ -13,9 +13,41 @@ public class IfStatementsTutorial : MonoBehaviour
     void Start()
     {
         // == is asking if the left equals the right
+        // = is that the left gets the right
         if(likesCoffee == true)
         {
             Debug.Log("Chloe Likes coffee");
+        }
+
+        if(likesCoffee == false)
+        {
+            Debug.Log("chloe dislikes coffee");
+        }
+        // is equal to
+        if(myName == "stacey")
+        {
+            Debug.Log("your name is stacey");
+        }
+        // != is not equal to
+        if (myName != "stacey")
+        {
+            Debug.Log("your name is chloe");
+            likesCoffee = false;
+        }
+
+        if(myFavouriteNumber == 17)
+        {
+            Debug.Log("is exactly 7");
+        }
+
+        if(myFavouriteNumber < 5)
+        {
+            Debug.Log("Your number is less than 5");
+        }
+
+        if(myFavouriteNumber > 5)
+        {
+            Debug.Log("your number is greater than 5");
         }
     }
 }
