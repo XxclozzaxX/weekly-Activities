5213548a43a9927a77306371ca4c5f52c9418f8c
chloe buckley <1017279@student.sae.edu.au>
2020-07-01 17:45:24 +1000

    week 3 class work

--- Assets/scripts/week_03/FunctionsWithParamaters.cs
+++ Assets/scripts/week_03/FunctionsWithParamaters.cs
@@ -18,8 +18,9 @@ public class FunctionsWithParamaters : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // an example of how to call a function
+        // an example of how to call a function with paramaters
         PrintFullName(myFirstName, myLastName, myAgeInY);
+        // an example of using an optional paramater
         PrintFullName("louise", "louisa");
     }
 
@@ -36,6 +37,11 @@ public class FunctionsWithParamaters : MonoBehaviour
     // the function name, all caps for words
     // finally the parentheses and curly braces
 
+    // inside of our parentheses i can declare rempoary variables which can store data
+    // from when the function is called
+    // this allowes me to reuse this fucntion more effectivly
+    // when we use the = sign here we can set default values for our paramaters
+    // just make sure that they are at the end of that paramater list
     public void PrintFullName(string FirstName, string LastName, int MyAge = -1)
     {
         Debug.Log(FirstName + " " + LastName);
@@ -58,6 +64,9 @@ public class FunctionsWithParamaters : MonoBehaviour
         int myAgeInD = 0;
 
         // calculates the users age in months, weeks and days
+        // here we notice i am using my paramater instead of my variable
+        // this allowes me to use whatever value i store in my paramater
+        // making my code more reuseable
         myAgeInM = MyAgeInYears * monthsInAY;
         myAgeInW = myAgeInM * weeksInAM;
         myAgeInD = myAgeInW * daysInAW;
---
+++ Assets/scripts/week_03/week3class.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class week3class : MonoBehaviour
+{
+    public string firstName = "chloe";
+    public string lastName = "buckley";
+    public int myAgeInY = 19;
+    public int myAgeInM = 0;
+    public int myAgeInW = 0;
+    public int myAgeInD = 0;
+    public int strength = 0;
+    public int agility = 0;
+    public int intellegance = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("my name is: " + firstName + " my last name is: " + lastName);
+
+        int monthsInYear = 12;
+        int weeksInAMonth = 4;
+        int daysInAWeek = 7;
+
+        myAgeInY = myAgeInY * monthsInYear;
+        myAgeInM = myAgeInY * monthsInYear;
+        myAgeInW = myAgeInM * weeksInAMonth;
+        myAgeInD = myAgeInW * daysInAWeek;
+
+        Debug.Log("my age is in different forms " + myAgeInY + " months " + myAgeInM + " weeks "
+            + myAgeInW + " days " + myAgeInD + " this is a lot");
+
+        PrintPowerLevel(strength, agility, intellegance);
+    }
+
+    // Update is called once per frame
+    public void PrintPowerLevel(int strength, int agility, int intellegance)
+    {
+        // small / big * 100
+        // return (int) 0;
+
+        strength = Random.Range(0, 20);
+        agility = Random.Range(0, 20);
+        intellegance = Random.Range(0, 20);
+
+        Debug.Log("my strength " + strength * 2);
+        Debug.Log("my agility " + agility * 1.5);
+        Debug.Log("my intellegance " + intellegance);
+    }
+}
