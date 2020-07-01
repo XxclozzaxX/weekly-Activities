bb86b8d29a6e34e375b5c22ed0960deb44c91938
chloe buckley <1017279@student.sae.edu.au>
2020-07-01 13:10:10 +1000

    basucs of functions tutorial completed

---
+++ Assets/scripts/week_03/BasicsOfFunctions.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicsOfFunctions : MonoBehaviour
+{
+    public string myFirstName = "chloe";
+    public string myLastName = "buckley";
+    public int myAgeInY = 19;
+    public int myAgeInM = 2;
+    public int myAgeInW = 0;
+
+    // this is a function called before start
+    public void Awake()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        // an example of how to call a function
+        PrintFullName();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PrintMyClassTime();
+    }
+    // assess type || return type || function name
+    public void PrintFullName()
+    {
+        Debug.Log(myFirstName + " " + myLastName);
+        // a function can be called inside of another function
+        PrintMyAge();
+    }
+
+    // the anatomy of a function follows
+    // the access type: public / private
+    // the return type: void - return nothing, just do instructions
+    // the function name, all caps for words
+    // finally the parentheses and curly braces
+
+    public void PrintMyAge()
+    {
+        Debug.Log("call print my name");
+        // tempoary variables
+        int monthsInAY = 12;
+        int weeksInAM = 4;
+        int daysInAW = 7;
+        int myAgeInD = 0;
+
+        // calculates the users age in months, weeks and days
+        myAgeInM = myAgeInY * monthsInAY;
+        myAgeInW = myAgeInM * weeksInAM;
+        myAgeInD = myAgeInW * daysInAW;
+        // and example of how debug.log effectivly in a function
+        Debug.Log("my age in days is: " + myAgeInD);
+        // prints out the users age in months, weeks and days
+        Debug.Log("my age is " + myAgeInY + " years old, which is " + myAgeInM + " in months," +
+            " which is " + myAgeInW + " in weeks, which is " + myAgeInD);
+    }
+    public void PrintMyClassTime()
+    {
+        Debug.Log("My class is wednesday at 15:00");
+    }
+
+}
---
+++ Assets/scripts/week_03/WeekTwoRecap.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeekTwoRecap : MonoBehaviour
+{
+    public string myName = "chloe";
+    public int myFaveInt = 17;
+    public float myFaveFloat = 5.4f;
+    public bool MyFaveBool = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // example of random range and float
+        myFaveInt = Random.Range(0, 11);
+        myFaveFloat = Random.Range(0f, 10f);
+
+        // temporary variables
+        string lastName = "buckley";
+        int myFirstCalc = 0;
+        int mySecondCalc = myFirstCalc;
+
+        // different examples of if statements
+        if (MyFaveBool == false)
+        {
+            Debug.Log("this is true");
+        }
+
+        if (myName == "chloe")
+        {
+            Debug.Log("this is true");
+        }
+
+        if (myFaveInt > 0)
+        {
+            Debug.Log("this is true");
+        }
+
+        if (myFaveFloat <= 100)
+        {
+            Debug.Log("this is true");
+        }
+
+        if (myFaveFloat == 1)
+        {
+            Debug.Log("this is true");
+        }
+
+        if (lastName == "buckley")
+        {
+            Debug.Log("this is true");
+        }
+        else
+        {
+            Debug.Log("this is false");
+        }
+
+        if (myName == "chloe")
+        {
+            Debug.Log("this is true");
+            if (lastName == "Buckley")
+            {
+                Debug.Log("your chloe");
+            }
+            else
+            {
+                Debug.Log("do something else");
+            }
+        }
+        else if (lastName == "buckley")
+        {
+            Debug.Log("were in else if");
+        }
+
+        /// && if any statement is false
+        /// this statement will not play out
+        if (myName != "chloe" && lastName == "buckley" && myFaveInt == 17)
+        {
+            Debug.Log(myName = " " + lastName);
+        }
+        /// this will only not play out
+        /// if both sides of the or are flase
+        if (myName != "chloe" || lastName == "buckley" && myFaveInt == 17)
+        {
+            Debug.Log(myName = " " + lastName);
+        }
+        /// use the parentheses to define what your conditions are
+        /// and also how many there are
+        if ((myName != "chloe" || lastName == "buckley") && myFaveInt == 17)
+        {
+            Debug.Log(myName = " " + lastName);
+        }
+    }
+}
+
