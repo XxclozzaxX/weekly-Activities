1d83fc1ae6846e96c567bc1406118904770a4eb4
chloe buckley <1017279@student.sae.edu.au>
2020-07-21 16:42:18 +1000

    Getting References to Script tutorial completed

---
+++ Assets/scripts/week_05/ScriptFinder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptFinder : MonoBehaviour
+{
+    public static ScriptFinder scriptFinderInstance;
+    // an example of a static instance
+    private ScriptToFind scriptToFind;
+    // a referance to the script to find
+    public Transform objectTransform;
+    // a referance to the object transform that has our script to find
+
+    private void Awake()
+    {
+        scriptFinderInstance = this;
+        // this keyword refers to this instance of this script
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scriptToFind = GetComponent<ScriptToFind>();
+        // this one will search the object for attached scripts
+        scriptToFind = FindObjectOfType<ScriptToFind>();
+        // this one will search the object transform referance for the
+        // script that it is attached to
+        scriptToFind = objectTransform.GetComponent<ScriptToFind>();
+        // this one will find the first instance of the script
+    }
+
+}
