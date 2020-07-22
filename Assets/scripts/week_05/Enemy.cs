016bbe346e16ef22d438772c28f102b76b805e41
chloe buckley <1017279@student.sae.edu.au>
2020-07-22 10:27:00 +1000

    Scope Get & Set Tutorial Completed

--- Assets/scripts/week_05/Enemy.cs
+++ Assets/scripts/week_05/Enemy.cs
@@ -9,8 +9,38 @@ public class Enemy : MonoBehaviour
     public string myLastName; // enemy last name
     public int health; // enemy health
     public int mana; // enemy mana
+    private string FullName; // private variable
+    // an example of a get and a set
+    // access type, data type and name || like a return function
+    public int Health
+    {
+        get
+        {
+            // i might want to debug or do something here
+            return health;
+        }
+        set
+        {
+            // what this value is set, change ui
+            // increse or decrese the death effect
+            health = value;
+        }
+    }
 
-    private string fullName;
+    public string fullName
+    {
+        get
+        {
+            Debug.Log("my namo is " + FullName);
+            return FullName;
+        }
+        set
+        {
+            Debug.Log(value);
+            FullName = value;
+        }
+    }
+
     // pravate and cant be seen in another class
 
     // sets the instance enemy class with some default values
@@ -20,5 +50,10 @@ public class Enemy : MonoBehaviour
         myLastName = "buckley";
         health = 100;
         mana = 100;
+        Health = 100;
+        // this is an example of using set 
+        fullName = "chloe";
+        // this is an example of get
+        Debug.Log("testing get " + fullName);
     }
 }
